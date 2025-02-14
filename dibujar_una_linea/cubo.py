cb481dddef9994a1534da5098370251f35a53bbc
ripOzziel <136388849+ripOzziel@users.noreply.github.com>
2025-02-13 17:17:34 -0800

    Integracion de codigo de la tortuga haciendo un cubo 3D

---
+++ dibujar_una_linea/cubo.py
@@ -0,0 +1,45 @@
+import turtle as t
+from random import random
+#cuadrado 1
+t.right (90)
+t.fd(100)
+t.right (90)
+t.fd(100)
+t.right (90)
+t.fd(100)
+t.right (90)
+t.fd(100)
+
+#conexion
+t.right(45)
+t.fd(110)
+t.left(45)
+#cuadrado 2
+t.right (90)
+t.fd(100)
+t.right (90)
+t.fd(100)
+t.right (90)
+t.fd(100)
+t.right (90)
+t.fd(100)
+
+t.right (90)
+t.fd(100)
+
+t.left(225)
+t.fd(110)
+
+t.left(45)
+t.fd(100)
+
+t.left(135)
+t.fd(110)
+
+t.left(135)
+t.fd(100)
+
+t.left(45)
+t.fd(110)
+
+input('con')
